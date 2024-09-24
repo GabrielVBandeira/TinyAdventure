226a56d49de4bff27bcb58ec49aec9b800d8bc3f
Gabriel <gabrielvbdev@gmail.com>
2024-09-23 21:10:40 -0300

    [FEAT] Adicionando fontes e menu

---
+++ tiny_swords/Management/Menu.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		foreach (Button button in GetTree().GetNodesInGroup("button"))
+		{
+			button.Pressed += () => OnButtonPressed(button.Name);
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	// Método chamado quando o botão é pressionado
+	private void OnButtonPressed(string buttonName)
+	{
+		switch (buttonName)
+		{
+			case "NewGame":
+				GetTree().ChangeSceneToFile("res://tiny_swords/Management/level.tscn");
+				break;
+
+			case "Quit":
+				GetTree().Quit();
+				break;
+		}
+	}
+}
